053e0eda072a9ede8685cc39193a92c255442857
william <willshinji@gmail.com>
2019-10-17 20:04:28 -0300

    add array Address in schema

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,8 +1,9 @@
 const users = [
-    {id: 1, name: "William", email: "william@mail.com"}
+    {id: 1, name: "William", email: "william@mail.com", address: [{nickname: "Home",street: 'Avenue A', zip: '12345', city: 'São José dos Campos'}, {nickname: "Apartment", street: "Street A", zip: '32145', city: 'São Paulo'}]}
 ]
 module.exports = {
     Query: {
-        users: () => users
+        users: () => users,
+        user: (_, {id}) => users[0]
     }
 }
